9b05c20000dab1aa50f669bf0bbbc38baea7a1e3
kian kheirani <kiankheirani@gmail.com>
2023-09-23 13:26:21 +0330

    'add-menue'

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { TfiClose } from "react-icons/tfi";
 import logo from "@/public/images/logo.svg";
+import { FiChevronRight } from "react-icons/fi";
+
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const navigationmenue = [
@@ -16,7 +19,7 @@ export default function Navigation() {
   return (
     <>
       <header className="py-7">
-        <div className="px-4 mx-auto ">
+        <div className="px-4 mx-auto container">
           <div className="flex justify-between items-center">
             <div>
               <Link href="/">
@@ -49,6 +52,39 @@ export default function Navigation() {
           </div>
         </div>
       </header>
+      {/*menu baar*/}
+      <div>
+        <div className="fixed w-screen h-screen z-[11] top-0 bg-black bg-opacity-50">
+          <div className="bg-white h-screen w-[380px] fixed z-[12] top-0 right-0 ">
+            <div className="flex items-center h-14 px-10 border-b ">
+              <button className="flex items-center space-x-3 ">
+                <TfiClose></TfiClose>
+                <span>Close</span>
+              </button>
+            </div>
+            <div className=" h-full py-3 px-10 pb-20 overflow-y-scroll scroll-smooth">
+              <ul className="block mb-7">
+                {navigationmenue.map((value, key) => {
+                  return (
+                    <li key={value.label}>
+                      <Link
+                        className="group flex py-2 duration-300 ease-in-out transition-all hover:text-primary items-center"
+                        href={value.href}
+                      >
+                        <span>{value.label}</span>
+                        <span className="opacity-0 ease-in-out duration-300  relative left-2 group-hover:left-4 group-hover:opacity-100 ">
+                          <FiChevronRight />
+                        </span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/*menu baar*/}
+      </div>
     </>
   );
 }
