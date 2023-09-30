2b37dc37fe622abbd8f008ac71355daf77410250
kian kheirani <kiankheirani@gmail.com>
2023-09-30 18:39:42 +0330

    'add-to'service'

--- app/components/Service.tsx
+++ app/components/Service.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const servicecotent = {
   heading: {
@@ -45,11 +46,26 @@ const servicecotent = {
     },
   ],
 };
+const elements = servicecotent.items.map((element) => {
+  return (
+    <div key={element.title} className="flex gap-5 ">
+      <div className="w-1/5 self-start bg-white shadow-[0_5px_20px_0px_rgba(0,0,0,0.2)] rounded-lg py-2 px-0 flex justify-center">
+        <Image alt="s" width={50} height={100} src={element.icon}></Image>
+      </div>
+      <div className="w-4/5">
+        <span className="font-bold inline-block text-primary mb-3">
+          {element.title}
+        </span>
+        <h2 className="text-body">{element.description}</h2>
+      </div>
+    </div>
+  );
+});
 
 export default function Service() {
   return (
     <section>
-      <div className="container bg-light py-10 mt-10 mx-auto ">
+      <div className=" bg-light py-10 mt-10  ">
         <div className="text-center mt-5">
           <span className="before:content-[''] relative before:w-0 before:hover:w-full before:transition-all before:duration-[1500ms] before:rounded-md py-1 px-2 ease-in before:absolute before:top-0 before:left-0 before:z-[-5] before:bottom-0 before:bg-yellow-500 z-50 font-semiboldl text-xl ">
             {servicecotent.heading.sub}
@@ -60,6 +76,10 @@ export default function Service() {
           <p className="w-1/2 px-[28px] text-body mx-auto py-5">
             {servicecotent.heading.des}
           </p>
+        </div>
+        {/*second  */}
+        <div className="py-16 px-10 gap-x-3 gap-y-6  lg:grid lg:grid-cols-3 flex flex-col">
+          {elements}
         </div>
       </div>
     </section>
